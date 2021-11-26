9bdc4867bf4a67728090ce61ddad909a1f3e1b5d
eminb <uslanyaramazov@gmail.com>
2021-11-27 02:36:55 +0300

    Drawing 8 committed

---
+++ src/OopPractise/DrawingA.java
@@ -0,0 +1,29 @@
+package OopPractise;
+
+public class DrawingA {
+    public static void main(String[] args){
+        String[][] letterArray = new String[7][5];
+
+        for (int i = 0; i < letterArray.length ; i++){
+            for (int j = 0; j < letterArray[i].length ; j++){
+                if ((i == 0) || (i == 3) || (i == 6)){
+                    letterArray[i][j] = " * ";
+                }
+                else if((j == 0) || (j == 4)){
+                    letterArray[i][j] = " * ";
+                }
+                else{
+                    letterArray[i][j] = "   ";
+                }
+            }
+            }
+        for (String[] row : letterArray){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+
+
+    }
+}

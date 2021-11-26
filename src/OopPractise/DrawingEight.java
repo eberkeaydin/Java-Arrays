6b838422f3338ad72a4e426903cdb602a92e42d8
eminb <uslanyaramazov@gmail.com>
2021-11-27 02:39:46 +0300

    Drawing 8 committed

--- src/OopPractise/DrawingEight.java
+++ src/OopPractise/DrawingEight.java
@@ -2,14 +2,14 @@ package OopPractise;
 
 public class DrawingEight {
     public static void main(String[] args){
-        String[][] letterArray = new String[7][5];
+        String[][] letterArray = new String[7][4];
 
         for (int i = 0; i < letterArray.length ; i++){
             for (int j = 0; j < letterArray[i].length ; j++){
                 if ((i == 0) || (i == 3) || (i == 6)){
                     letterArray[i][j] = " * ";
                 }
-                else if((j == 0) || (j == 4)){
+                else if((j == 0) || (j == 3)){
                     letterArray[i][j] = " * ";
                 }
                 else{
